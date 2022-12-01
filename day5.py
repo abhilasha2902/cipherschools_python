1b1e9f13df8fb740a388329c4bb52d189500ad19
abhilasha2902 <119339978+abhilasha2902@users.noreply.github.com>
2022-12-01 17:51:24 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,12 @@
+# global variables 
+# variables which are created outside the function
+#they can be used outside the fun.
+
+ 
+def myfunc():
+    x = 'peter'
+    print("my name is " + x)
+    print(x) 
+
+    myfunc() 
+    ## we write comments in pythonby using a #
